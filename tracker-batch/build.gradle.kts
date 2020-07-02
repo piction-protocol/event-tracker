c5a555058b0a3719b009d813c3c0cd7872317e6f
sk.kwon <sk.kwon@battleent.com>
2020-07-02 16:41:40 +0900

    Update caver java

--- tracker-batch/build.gradle.kts
+++ tracker-batch/build.gradle.kts
@@ -3,7 +3,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.flywaydb:flyway-core")
-    implementation("com.klaytn.caver:core:1.0.3")
+    implementation("com.klaytn.caver:core:1.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
