5cd82ed0846bceed03357f10a69350957299c857
sk.kwon <sk.kwon@battleent.com>
2020-07-07 15:02:53 +0900

    Update event repository

--- tracker-common/src/main/kotlin/network/piction/tracker/common/repositories/EventRepository.kt
+++ tracker-common/src/main/kotlin/network/piction/tracker/common/repositories/EventRepository.kt
@@ -1,5 +1,6 @@
 package network.piction.tracker.common.repositories
 
+import network.piction.tracker.common.entities.ContractEntity
 import network.piction.tracker.common.entities.EventEntity
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
@@ -9,5 +10,5 @@ import org.springframework.stereotype.Repository
 @Repository
 interface EventRepository : JpaRepository<EventEntity, Long> {
     fun findByContractIdAndId(contractId: Long, eventId: Long): EventEntity?
-    fun findByContractId(pageable: Pageable, contractId: Long): Page<EventEntity>
+    fun findByContract(pageable: Pageable, contract: ContractEntity): Page<EventEntity>
 }
