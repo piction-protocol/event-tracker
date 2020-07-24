e6dddf4d0a5465d72a6a8f4be07a43c276a29d42
dmbyun <dm.byun@battleent.com>
2020-07-24 19:34:26 +0900

    update mainclass

--- tracker-api/build.gradle.kts
+++ tracker-api/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 application {
-    mainClass.set("network.piction.tracker.TrackerApiApplication")
+    mainClassName("network.piction.tracker.TrackerApiApplication")
 }
 
 dependencies {
